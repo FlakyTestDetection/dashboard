ff9b37270700af517b48a1e1585bc937ea14e1d9
Jonathan Bell <jbell@cs.columbia.edu>
2017-05-06 15:28:12 -0400

    travis config

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $encrypted_7d9c2b09c3a5_key -iv $encrypted_7d9c2b09c3a5_iv -in travis_key.enc -out travis_key -d
+
+chmod 600 ../travis_key
+eval `ssh-agent -s`
+ssh-add travis_key
+
+npm run build
+
