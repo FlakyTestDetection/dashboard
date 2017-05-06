2ce1e661372da406b47a9e34e0a5e626dfc01d29
Jonathan Bell <jbell@cs.columbia.edu>
2017-05-06 15:34:48 -0400

    .

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $encrypted_7d9c2b09c3a5_key -iv $encrypted_7d9c2b09c3a5_iv -in travis_key.enc -out travis_key -d
 
+git config --global user.email "jon@jonbell.net"
+git config --global user.name "Jonathan Bell"
 chmod 600 travis_key
 eval `ssh-agent -s`
 ssh-add travis_key
